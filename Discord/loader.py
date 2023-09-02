15d182cfe86195c7218f90d21e03fb7c4a72647e
auth-xyz <smmc.auth@gmail.com>
2023-09-02 13:41:10 -0400

    Changed path to Discord/Legacy

--- Discord/loader.py
+++ Discord/loader.py
@@ -5,35 +5,15 @@ class CommandLoader:
         self.client = client
 
     def load_commands(self):
-        path = "Discord/Commands"
+        path = "Discord/Legacy"
         for filename in os.listdir(path):
             if not filename:
-                return print("[discord.loader] : no module found, skipping...")
+                return print("[andromeda.legacy] : no module found, skipping...")
             
             if filename.endswith(".py") and filename != "__init__.py":
-                module = f"Discord.Commands.{filename[:-3]}"
+                module = f"Discord.Legacy.{filename[:-3]}"
                 try:
                     self.client.load_extension(module)
-                    print(f"[discord.loader] : loaded {module}")
+                    print(f"[andromeda.legacy] : loaded {module}")
                 except Exception as e:
-                    print(f"[discord.loader] : failed to load {module}: \n{e}")
-
-
-class EventLoader:
-    def __init__(self, client):
-        self.client = client
-
-    def load_modules(self):
-        path = "Discord/Modules"
-        for filename in os.listdir(path):
-            if not filename:
-                return print("[discord.loader.modules] : no module found, skipping...")
-            
-            if filename.endswith(".py") and filename != "__init__.py":
-                module = f"Discord.Modules.{filename[:-3]}"
-                try:
-                    self.client.load_extension(module)
-                    print(f"[discord.loader.modules] : loaded {module}")
-                except Exception as e:
-                    print(f"[discord.loader.modules] : failed to load {module}: \n{e}")
-
+                    print(f"[andromeda.legacy] : failed to load {module}: \n{e}")
