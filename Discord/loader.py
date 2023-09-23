82ab11f86337aafc02d2c1d33914d8615cf55527
auth-xyz <authenticover@gmail.com>
2023-09-22 23:56:38 -0400

    Introduced EventLoader to loader.py

--- Discord/loader.py
+++ Discord/loader.py
@@ -1,4 +1,13 @@
 import os
+import dotenv
+import nextcord
+
+from Utils.welcome import generate_custom_message
+from Utils.database import Database
+
+dotenv.load_dotenv()
+db = Database(os.getenv("DB_L"), "Dataset", "Chatlogs")
+db.connect()
 
 
 class LegacyLoader:
@@ -10,7 +19,7 @@ class LegacyLoader:
         for filename in os.listdir(path):
             if not filename:
                 return print("[andromeda.loader] : no module found, skipping...")
-            
+
             if filename.endswith(".py") and filename != "__init__.py":
                 module = f"Discord.Legacy.{filename[:-3]}"
                 try:
@@ -38,3 +47,107 @@ class InteractionLoader:
                 except Exception as e:
                     print(f"[andromeda.loader] : failed to load {module}\n[andromeda.error] {e}")
 
+
+class EventLoader:
+    def __init__(self, client):
+        self.client = client
+
+    async def on_channel_update(self, before, after):
+        channel = self.client.get_channel(1070176260891889725)
+        text = f"Channel {before.name} was changed"
+        chan = set(before) - set(after)
+        desc = f"```\nChanges:\n{chan}\n```"
+
+        embed = nextcord.Embed(
+            title=text,
+            description=desc,
+            colour=nextcord.Color.blue()
+        )
+        await channel.send(embed=embed)
+
+    async def on_message_edit(self, before: nextcord.Message, after: nextcord.Message):
+        channel = self.client.get_channel(1070176260891889725)
+        title = f"Edited Message in {before.channel.name}"
+        author = before.author or after.author
+
+        if before.content != after.content and not author.bot:
+            desc = f"User {author.display_name} edited a message.\n```\nFrom: {before.content}\n\nTo: {after.content}\n```"
+
+            embed = nextcord.Embed(
+                title=title,
+                description=desc,
+                color=nextcord.Color.yellow(),
+            )
+            embed.set_thumbnail(author.display_avatar)
+            await channel.send(embed=embed)
+
+    async def on_message_delete(self, message: nextcord.Message):
+        if message.author.bot:
+            return
+
+        channel = self.client.get_channel(1070176260891889725)
+        title = f"Deleted Message in {message.channel.name}"
+        desc = f"User {message.author.display_name} deleted a message\n```\n{message.content}\n```"
+
+        embed = nextcord.Embed(
+            title=title,
+            description=desc,
+            colour=nextcord.Color.red()
+        )
+        embed.set_thumbnail(message.author.display_avatar)
+        await channel.send(embed=embed)
+
+    async def on_member_join(self, member: nextcord.Member):
+        ids = [1070217307143536700, 1070216214871294022]
+        sid = 521850636321423371
+        serv = self.client.get_guild(sid)
+
+        if serv:
+            for rid in ids:
+                role = serv.get_role(rid)
+                if role:
+                    await member.add_roles(role)
+
+    async def on_member_update(self, before, after: nextcord.Member):
+        added_roles = set(after.roles) - set(before.roles)
+
+        if added_roles:
+            for role in added_roles:
+                if role.id == 1070171459202322442:
+                    channel = self.client.get_channel(1070173038194216990)
+                    msg = generate_custom_message(
+                        f"Welcome to Nebula, {after.display_name}\nHave an awesome time here!",
+                        table_width=40, center_text=False)
+                    embed = nextcord.Embed(
+                        title=f"Member {after} joined!",
+                        description=f"```\n{msg}\n```"
+                    )
+                    embed.set_thumbnail(after.display_avatar)
+                    await channel.send(embed=embed, content=f"{after.mention}")
+
+    async def on_ready(self):
+        cl = LegacyLoader(self.client)
+        il = InteractionLoader(self.client)
+
+        cl.load_commands()
+        print("\n")
+        il.load_interaction()
+
+        try:
+            await self.client.sync_all_application_commands()
+        except Exception as e:
+            print(f"[discord.error] : {e}")
+
+        print("\n[discord.main] : successfully established connection with discord.\n")
+
+    @staticmethod
+    async def on_message(message: nextcord.Message):
+        if message.author.bot:
+            return
+
+        word = message.content.lower()
+        document = db.find_document({"query": word})
+
+        if document:
+            response = document["value"]
+            await message.reply(response)
--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import nextcord
 from dotenv import load_dotenv
 from nextcord.ext import commands
 
-from Utils.welcome import generate_custom_message
 from Utils.database import Database
-from Discord.loader import LegacyLoader, InteractionLoader
+from Discord.loader import LegacyLoader, InteractionLoader, EventLoader
 
 load_dotenv()
 
@@ -14,6 +13,7 @@ intents = nextcord.Intents.all()
 client = commands.Bot("!", intents=intents)
 cl = LegacyLoader(client)
 il = InteractionLoader(client)
+el = EventLoader(client)
 
 db = Database(os.getenv("DB_L"), "Dataset", "Chatlogs")
 db.connect()
@@ -21,111 +21,37 @@ db.connect()
 
 @client.event
 async def on_ready():
-    cl.load_commands()
-    print("\n")
-    il.load_interaction()
-
-    try:
-        await client.sync_all_application_commands()
-    except Exception as e:
-        print(f"[discord.error] : {e}")
-
-    print("\n[discord.main] : successfully established connection with discord.\n")
+    await el.on_ready()
 
 
 @client.event
 async def on_message(message):
-    if message.author.bot:
-        return
-
-    word = message.content.lower()
-    document = db.find_document({"query": word})
-
-    if document:
-        response = document["value"]
-        await message.reply(response)
+    await el.on_message(message)
 
 
 @client.event
 async def on_member_update(before, after: nextcord.Member):
-    added_roles = set(after.roles) - set(before.roles)
-
-    if added_roles:
-        for role in added_roles:
-            if role.id == 1070171459202322442:
-                channel = client.get_channel(1070173038194216990)
-                msg = generate_custom_message(f"Welcome to Nebula, {after.display_name}\nHave an awesome time here!",
-                                              table_width=40, center_text=False)
-                embed = nextcord.Embed(
-                    title=f"Member {after} joined!",
-                    description=f"```\n{msg}\n```"
-                )
-                embed.set_thumbnail(after.display_avatar)
-                await channel.send(embed=embed, content=f"{after.mention}")
+    await el.on_member_update(before, after)
 
 
 @client.event
 async def on_member_join(member: nextcord.Member):
-    ids = [1070217307143536700, 1070216214871294022]
-    sid = 521850636321423371
-    serv = client.get_guild(sid)
-
-    if serv:
-        for rid in ids:
-            role = serv.get_role(rid)
-            if role:
-                await member.add_roles(role)
+    await el.on_member_join(member)
 
 
 @client.event
 async def on_message_delete(message: nextcord.Message):
-    if message.author.bot:
-        return
-
-    channel = client.get_channel(1070176260891889725)
-    title = f"Deleted Message in {message.channel.name}"
-    desc = f"User {message.author.display_name} deleted a message\n```\n{message.content}\n```"
-
-    embed = nextcord.Embed(
-        title=title,
-        description=desc,
-        colour=nextcord.Color.red()
-    )
-    embed.set_thumbnail(message.author.display_avatar)
-    await channel.send(embed=embed)
+    await el.on_message_delete(message)
 
 
 @client.event
 async def on_message_edit(before: nextcord.Message, after: nextcord.Message):
-    channel = client.get_channel(1070176260891889725)
-    title = f"Edited Message in {before.channel.name}"
-    author = before.author or after.author
-
-    if before.content != after.content and not author.bot:
-        desc = f"User {author.display_name} edited a message.\n```\nFrom: {before.content}\n\nTo: {after.content}\n```"
-
-        embed = nextcord.Embed(
-            title=title,
-            description=desc,
-            color=nextcord.Color.yellow(),
-        )
-        embed.set_thumbnail(author.display_avatar)
-        await channel.send(embed=embed)
+    await el.on_message_edit(before, after)
 
 
 @client.event
 async def on_channel_update(before, after):
-    channel = client.get_channel(1070176260891889725)
-    text = f"Channel {before.name} was changed"
-    chan = set(before) - set(after)
-    desc = f"```\nChanges:\n{chan}\n```"
-
-    embed = nextcord.Embed(
-        title=text,
-        description=desc,
-        colour=nextcord.Color.blue()
-    )
-    await channel.send(embed=embed)
+    await el.on_channel_update(before, after)
 
 
 client.run(os.getenv("TOKEN"))
