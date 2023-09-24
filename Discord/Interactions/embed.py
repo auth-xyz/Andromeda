ff8a30d79768209194fdc02b2eac412d813bfdab
auth-xyz <authenticover@gmail.com>
2023-09-23 20:10:37 -0400

    Added embeds for rules

---
+++ Discord/Interactions/embed.py
@@ -0,0 +1,69 @@
+import nextcord
+
+from nextcord import Interaction, SlashOption, Embed
+from nextcord.ext import commands
+from Discord.discord_rules import drules, drules2
+
+intents = nextcord.Intents.all()
+client = commands.Bot("!", intents=intents)
+
+
+class intEmbed(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @client.slash_command(name="embed", description="Embed generator")
+    @commands.has_permissions(ban_members=True)
+    async def embed(self, interaction: Interaction, embed: str = SlashOption(required=True, choices=[
+        "rules",
+        "nsfw_rules",
+        "andromeda_usage",
+        "voice_rules",
+    ])):
+        rules_embed = self.embed_creator(
+            title="",
+            desc=f"{drules}",
+            color=nextcord.Color.dark_purple()
+        )
+        rules_2 = self.embed_creator(
+            title="",
+            desc=f"{drules2}",
+            color=nextcord.Color.dark_purple()
+        )
+        rules_3 = self.embed_creator(
+            title="Accept Rules",
+            desc="""
+            NOW THAT YOU'VE READ & UNDERSTAND THESE RULES, PLEASE REACT TO THIS MESSAGE TO GET MEMBER ROLE STATUS, 
+            TO VIEW ALL CHANNELS ALSO VISIT #【🔘】self-assign-roles TO GET YOUR PERSONIFIED ROLES.
+            """,
+            color=nextcord.Color.dark_purple()
+        )
+        nsfw_embed = self.embed_creator(
+            title="NSFW Rules",
+            desc="```\n\n```",
+            color=nextcord.Color.red()
+        )
+
+        if embed == "rules":
+            await interaction.send(embed=rules_embed)
+            await interaction.send(embed=rules_2)
+            await interaction.send(embed=rules_3)
+        elif embed == "nsfw_rules":
+            await interaction.send(embed=nsfw_embed)
+
+    @staticmethod
+    def embed_creator(title, desc, color):
+        text = title
+        desc = desc
+
+        embed = Embed(
+            title=text,
+            description=desc,
+            colour=color
+        )
+
+        return embed
+
+
+def setup(bot):
+    bot.add_cog(intEmbed(bot))
---
+++ Discord/discord_rules.py
@@ -0,0 +1,89 @@
+drules = """
+▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+
+1. **Be respectful**
+`This means no mean, rude, or harassing comments. Treat others the way you want to be treated.`
+
+2. **No spamming**
+`Do not send a lot of small messages right after each other. These disrupt chat and make it hard to scroll through the server. Please keep your messages at least 5 words long while chatting.`
+
+3. **No pornographic/adult/other NSFW material**
+`The idea behind the server is to provide a safe place for us to share art, videos, and other kinds of creative material – not to share the aforementioned NSFW material. IT IS still allowed in the apropriated channels for it of course!`
+
+4. **No offensive names and profile pictures**
+`Keep your names and profile picture appropriate.`
+
+5. **Server Raiding**
+`Server raiding is against the Discord terms of service. Any attempt to circumvent or bypass them can result in a permanent ban.`
+
+6. **Threats are forbidden**
+`Threats are prohibited and disallowed.`
+
+7. **Follow the Discord Community Guidelines**
+`Respect the general Discord Community guidelines.`
+
+8. **Don’t share your personal information**
+`Do not share your personal information or the personal information of other users without their consent. This includes phone numbers, addresses, and any other sensitive information.`
+
+9. **Don’t share illegal or pirated content**
+`Do not share links to illegal or pirated content, such as copyrighted material or stolen software. This is against the Discord terms of service and can result in a ban from the server.`
+
+10. **Avoid false or misleading information**
+`Make sure to fact-check any information that you share and only share reliable sources. Spreading false or misleading information can be harmful and undermines the trust and integrity of the community.`
+
+11. **Follow the channel-specific guidelines**
+`Each channel in your server should have specific guidelines for the type of content that is allowed. Make sure to follow these guidelines and respect the purpose of each channel.`
+
+12. **Respect others’ privacy**
+`Do not invade the privacy of other users by asking for or sharing their personal information, such as real names, addresses, or social media accounts without their consent.`
+
+13. No excessive self-promotion
+`Share your work in moderation and only in designated channels.`
+
+14. **Keep political and religious discussions civil and respectful**
+`Or avoid them altogether if they are not relevant to the server’s purpose.`
+
+15. **Do not intentionally disrupt conversations or create a hostile environment for other users**
+`This includes trolling, flaming, or excessive arguing.`
+
+16. **No public shaming or call-outs**
+`If you have an issue with another user or their behavior, address it privately or report it to a staff member. Do not publicly shame or call out other users in the server.`
+
+17. **Do not abuse server bots**
+`Only use server bots for their intended purposes and do not spam or misuse their commands.`
+
+18. **Do not excessively use caps or emojis in your messages**
+`This can be disruptive and make the chat difficult to follow.`
+
+19. **No sharing or promoting illegal activities**
+`Do not share or promote any illegal activities, such as hacking, phishing, or drug use.`
+
+"""
+
+drules2 = """
+20. **No self-destructive behavior or encouragement of self-harm**
+`Do not encourage, glorify, or engage in self-destructive behavior or self-harm. This includes sharing graphic content related to self-harm or discussing personal experiences in a way that may trigger others. #【🧸】venting is the only channel you can share whats on your soul.`
+
+21. **Do not share graphic or disturbing content**
+`Such as gore, violence, or explicit sexual content.`
+
+22. **No promoting or discussing drugs, alcohol, or other substances**
+`Avoid discussing or promoting the use of drugs, alcohol, or other substances that may be harmful or illegal.`
+
+23. **No soliciting or begging for money, items, or services**
+`Do not solicit or beg for money, items, or services from other users.`
+
+24. **No blackmail, threats, or harassment**
+`Do not engage in blackmail, threats, or harassment of any kind, whether in public or private messages.`
+
+25. **No sharing of personal or private messages without consent**
+`Do not share the contents of personal or private messages with others without the consent of all involved parties.`
+
+26. **Do not evade bans or mutes**
+`If you have been banned or muted, do not attempt to evade the punishment by creating alternate accounts or otherwise circumventing the restrictions.`
+
+*The Admins and Moderators will Mute/Kick/Ban per discretion. If you feel mistreated DM an Admin and we will resolve the issue.*
+*All Channels will have pinned messages explaining what they are there for and how everything works. If you don’t understand something, feel free to ask!*
+
+▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
+"""
