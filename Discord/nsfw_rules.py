ef09e3843f3541fc959d3e9cfa712e8402a81049
auth-xyz <authenticover@gmail.com>
2023-09-23 20:27:26 -0400

    Fixed nsfw rules

--- Discord/nsfw_rules.py
+++ Discord/nsfw_rules.py
@@ -1,24 +1,24 @@
 nsfw_rules = """
 ▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰▰
 
-1. `This shouldn't even be stated, if any sort of character does not look mature you will be banned.`
-`All characters must be 18+ and/or look like an adult.`
+`1.` **This shouldn't even be stated, if any sort of character does not look mature you will be banned.**
+**All characters must be 18+ and/or look like an adult.**
 
-2. `No bestiality or any kind of animal pornography!`
+`2.` **No bestiality or any kind of animal pornography!**
 
-3. `No furry content.`
+`3.` **No furry content.**
 
-4. `No pregnant characters.`
+`4.` **No pregnant characters.**
 
-5. `No vore or anything related to the that, get your filthy kinks out.`
+`5.` **No vore or anything related to the that, get your filthy kinks out.**
 
-6. `No gore, vomit or anything that can be mildly related to those topics.`
+`6.` **No gore, vomit or anything that can be mildly related to those topics.**
 
-7. `Do not post any image that depicts a character hurting themselves. That includes, self-hurt or suicide.`
+`7.` **Do not post any image that depicts a character hurting themselves. That includes, self-hurt or suicide.**
 
-8. `Content related to non-consensual sex will be deleted and the user will be banned `
+`8.` **Content related to non-consensual sex will be deleted and the user will be banned, **
 
-9. `Refrain from sending text messages on the image channels.
+`9.` **Refrain from sending text messages on the image channels.**
 
 *If you have further suggestions/ideas for this section let us know in 【🚫】nsfw-suggestion*
 
